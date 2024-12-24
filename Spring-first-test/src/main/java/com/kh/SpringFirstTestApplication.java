3a4567e703bb02654702a3db9e127bab75e1ca02
2-H-J <govl7844@gmail.com>
2024-12-24 10:32:27 +0900

    Spring Boot

---
+++ Spring-first-test/src/main/java/com/kh/SpringFirstTestApplication.java
@@ -0,0 +1,13 @@
+package com.kh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFirstTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFirstTestApplication.class, args);
+	}
+
+}
